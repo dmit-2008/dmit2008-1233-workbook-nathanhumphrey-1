55927bb73db9a96386919c96f523bc69291cbcb7
Nathan Humphrey <NathanH@nait.ca>
2024-05-13 09:05:52 -0600

    Complete review break activities example

--- review-break-activities-example/js/index.js
+++ review-break-activities-example/js/index.js
@@ -7,20 +7,41 @@ HTML for list topic list item
 </li>
 */
 
-const topicForm = document.querySelector('.new-topic-form');
-const topicsList = document.querySelector('.topics-list');
+// wrap main logic in a block to prevent global scoping
+{
+  const topicForm = document.querySelector('.new-topic-form');
+  const topicList = document.querySelector('.topics-list');
 
-topicForm.addEventListener('submit', (event) => {
-  event.preventDefault();
+  /**
+   * Adds a li element to the topic list.
+   * @param {string} topicText - the new text for the topic to add
+   */
+  const addNewTopic = (topicText) => {
+    const li = `
+            <li class="list-group-item">
+                ${topicText}
+            </li>`;
 
-  const topicEl = topicForm.elements['new-topic'];
-  const topicText = topicEl.value.trim();
+    topicList.innerHTML += li;
+  };
 
-  const topicLi = `
-    <li class="list-group-item">
-        ${topicText}
-    </li>
-  `;
+  topicForm.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-  topicsList.innerHTML += topicLi;
-});
+    // get the input value
+    const topicText = topicForm.elements['new-topic'].value.trim();
+
+    // validate the input
+    if (topicText === '') {
+      topicForm.elements['new-topic'].classList.add('is-invalid');
+    } else {
+      topicForm.elements['new-topic'].classList.remove('is-invalid');
+      addNewTopic(topicText);
+
+      // clear the form now that the topic has been added
+      topicForm.reset();
+      // focus the input so the user can enter another topic
+      topicForm.elements['new-topic'].focus();
+    }
+  });
+}
