a1970d27c5885706d64dabfd5f6dc87a3d6f2afa
Nathan Humphrey <NathanH@nait.ca>
2024-06-11 18:51:09 -0600

    Update weather app lift state up

--- weather-app-example/components/WeatherForm.js
+++ weather-app-example/components/WeatherForm.js
@@ -1,11 +1,30 @@
 import { GetApp } from '@mui/icons-material';
 import { Box, Button, TextField, Typography } from '@mui/material';
 
-export default function WeatherForm() {
+export default function WeatherForm({ weatherCallback }) {
+  // TODO: implement state for the input value
+  // and render an error if the input is empty
+  // (NOTE: check error and helperText props for
+  // TextField)
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+
+    const location = evt.target.elements['location'].value.trim();
+
+    if (location === '') {
+      console.error('Location cannot be empty');
+    } else {
+      console.log(`Location: ${location}`);
+      // Fetch the weather data ...
+      weatherCallback('DATA from weather form...');
+    }
+  }
+
   return (
     <>
       <Typography variant="h2">Weather Form</Typography>
-      <Box component="form">
+      <Box component="form" onSubmit={handleSubmit}>
         <TextField
           id="outlined-basic"
           name="location"
--- weather-app-example/pages/index.js
+++ weather-app-example/pages/index.js
@@ -1,11 +1,22 @@
+import { useState } from 'react';
+import { Box, Container, Typography } from '@mui/material';
 import WeatherForm from '@/components/WeatherForm';
-import { Container, Typography } from '@mui/material';
 
 export default function Home() {
+  const [weatherData, setWeatherData] = useState(null);
+
   return (
     <Container maxWidth="lg">
       <Typography variant="h1">Weather Application</Typography>
-      <WeatherForm />
+      <WeatherForm weatherCallback={setWeatherData} />
+
+      <Box sx={{ mt: 2 }}>
+        {weatherData ? (
+          <Typography>{weatherData}</Typography>
+        ) : (
+          <Typography>Enter a location to search</Typography>
+        )}
+      </Box>
     </Container>
   );
 }
