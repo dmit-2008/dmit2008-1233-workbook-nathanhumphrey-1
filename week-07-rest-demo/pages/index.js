d47c1092c7e101bb78c01526d571b8f86e9dde1a
Nathan Humphrey <NathanH@nait.ca>
2024-06-18 18:52:08 -0600

    Complete week 07 day 01

--- week-07-rest-demo/pages/index.js
+++ week-07-rest-demo/pages/index.js
@@ -4,19 +4,30 @@ import {
   Box,
   Button,
   Container,
+  LinearProgress,
+  List,
+  ListItem,
+  ListItemText,
   Toolbar,
   Typography,
 } from '@mui/material';
+import { QuoteManager } from '@/utils/quote-manager';
 
 export default function Home() {
   const QUOTE_URL = 'https://api.quotable.io/random';
 
   const [randomQuote, setRandomQuote] = useState(null);
+  const [savedQuotes, setSavedQuotes] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   function getRandomQuote() {
+    setIsLoading(true);
+    setRandomQuote(null);
+
     fetch(QUOTE_URL)
       .then((res) => res.json())
       .then((json) => {
+        setIsLoading(false);
         setRandomQuote({ author: json.author, quote: json.content });
       });
   }
@@ -30,21 +41,51 @@ export default function Home() {
       </AppBar>
       <Container maxWidth="lg" component="section">
         <Typography variant="h1">Random Quotes</Typography>
-        <Box mt={4}>
+        <Box mt={4} mb={4}>
           <Button variant="contained" onClick={getRandomQuote}>
             Get a Quote
           </Button>
         </Box>
         <hr />
         <Box mt={4}>
+          {isLoading && <LinearProgress />}
           {randomQuote && (
             <Box>
-              <Typography variant="h2">A Random Quote</Typography>
-              <Typography>
+              <Typography variant="h2" mb={2}>
+                A Random Quote
+              </Typography>
+              <Typography mb={2}>
                 {randomQuote.quote} - {randomQuote.author}
               </Typography>
+              <Button
+                variant="contained"
+                onClick={async () => {
+                  await QuoteManager.saveQuote(randomQuote);
+                }}
+              >
+                Save Quote
+              </Button>
             </Box>
           )}
+        </Box>
+        <Box mt={4}>
+          <Typography variant="h2">Previously Saved Quotes</Typography>
+          <Button
+            variant="contained"
+            onClick={async () => {
+              setSavedQuotes(await QuoteManager.getSavedQuotes());
+            }}
+          >
+            Fetch Saved Quotes
+          </Button>
+          <List>
+            {savedQuotes &&
+              savedQuotes.map((q) => (
+                <ListItem key={q.id}>
+                  <ListItemText primary={q.quote} secondary={q.author} />
+                </ListItem>
+              ))}
+          </List>
         </Box>
       </Container>
     </Box>
--- week-07-rest-demo/utils/quote-manager.js
+++ week-07-rest-demo/utils/quote-manager.js
@@ -1,6 +1,11 @@
 const BASE_URL = 'http://localhost:3001/quotes';
 
 const QuoteManager = {
+  getSavedQuotes: async function () {
+    const res = await fetch(BASE_URL);
+    return await res.json();
+  },
+
   saveQuote: async function (quote) {
     if (!quote.quote || !quote.author) {
       throw new Error('Incompatible type');
