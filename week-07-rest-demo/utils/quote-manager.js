153e6e141d2da5bf9d4147f10be1c8a430ec20fe
Nathan Humphrey <NathanH@nait.ca>
2024-06-18 18:57:30 -0600

    Added QuoteManager comments

--- week-07-rest-demo/utils/quote-manager.js
+++ week-07-rest-demo/utils/quote-manager.js
@@ -1,11 +1,23 @@
 const BASE_URL = 'http://localhost:3001/quotes';
 
+/**
+ * Manages the 'quotes' API with the backend
+ */
 const QuoteManager = {
+  /**
+   * Fetches all quotes from the backend
+   * @returns {Promise<[]>} an array of quote objects
+   */
   getSavedQuotes: async function () {
     const res = await fetch(BASE_URL);
     return await res.json();
   },
 
+  /**
+   * Creates a new quote in the backend
+   * @param {{quote, author}} quote
+   * @returns {Promise<{quote, author, id}>} the newly added quote with id
+   */
   saveQuote: async function (quote) {
     if (!quote.quote || !quote.author) {
       throw new Error('Incompatible type');
